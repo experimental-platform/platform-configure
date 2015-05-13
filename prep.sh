aa1721c3eef2e2c90b52812d18bc1e5bf707f76e
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-05-13 15:30:03 +0200

    Do *not* delete directories.

--- prep.sh
+++ prep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /data/ ! -name "update-protonet*" -delete && find /services/ ! -name "update-protonet*" -type f -exec cp {} /data \;
+find /data/ ! -name "update-protonet*" -type f -o -type l -delete && find /services/ ! -name "update-protonet*" -type f -exec cp {} /data \;
