bdbc9cb841c726e02706ac7920ed5a5a04e135d6
Andreas Litt <andreas.litt@gmail.com>
2015-06-08 14:13:46 +0200

    delete all service files, not only links

--- prep.sh
+++ prep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /data/ ! -name "update-protonet*" -type f -o -type l -delete && find /services/ ! -name "update-protonet*" -type f -exec cp {} /data \;
+find /data/ ! -name "update-protonet*" -type f -delete -o -type l -delete && find /services/ ! -name "update-protonet*" -type f -exec cp {} /data \;
