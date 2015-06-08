0e6474b5d24424c6cf06de223d0ffa7bf54096ce
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-08 17:44:55 +0200

    DEBUG

--- prep.sh
+++ prep.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo -e "\n\nDEBUG START:\n"
+ls -la /data
+echo -e "\n\nDEBUG END\n"
 
 find /data/ ! -name "update-protonet*" -type f -delete -o -type l -delete && find /services/ ! -name "update-protonet*" -type f -exec cp {} /data \;
