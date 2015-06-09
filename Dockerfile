32c732ddbaa2c118564579c2ee42ae1377d04cce
Andreas Litt <andreas.litt@gmail.com>
2015-06-09 12:08:48 +0200

    try new way to add scripts

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM dockerregistry.protorz.net/ubuntu:latest
 
 ADD services /services
+ADD update-images-protonet.sh /update-images-protonet.sh
 ADD prep.sh /prep.sh
 
 CMD [ "/prep.sh" ]
--- prep.sh
+++ prep.sh
@@ -4,3 +4,5 @@ ls -la /data
 echo -e "\n\nDEBUG END\n"
 
 find /data/ ! -name "update-protonet*" -type f -delete -o -type l -delete && find /services/ ! -name "update-protonet*" -type f -exec cp {} /data \;
+
+cp /update-images-protonet.sh /data/update-images-protonet.sh
