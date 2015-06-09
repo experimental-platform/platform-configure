c8a5033b9d7291a8b8f21f6257eb19384fe0ec4b
Andreas Litt <andreas.litt@gmail.com>
2015-06-09 15:39:37 +0200

    new update mechanism for docker images

--- update-images-protonet.sh
+++ update-images-protonet.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo "jup"
+DOCKER=$(which docker)
+
+update_image() {
+  local image=$1
+  $DOCKER tag -f $image:latest $image:previous
+
+  $DOCKER pull $image:latest && $DOCKER save $image:latest > /dev/null
+  for layer in $(docker history --no-trunc $image:latest | tail -n +2 | awk '{ print $1 }'); do
+    if [[ ! -e /var/lib/docker/overlay/$layer || ! -e /var/lib/docker/graph/$layer ]]; then
+      echo "Layer '$layer' of '$image' missing. Switching to previous version."
+      $DOCKER tag -f $image:previous $image:latest
+      break 2
+    fi
+  done
+}
+for image in $(docker images | tail -n +2 | awk '/dockerregistry\.protorz\.net\/.+\s+latest/ { print $1 }'); do
+  (update_image $image)
+done
