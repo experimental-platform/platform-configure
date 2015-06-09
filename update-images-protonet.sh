0acb1b06bc67b1f2c6c0fcd1c6a9af00a464ace2
Andreas Litt <andreas.litt@gmail.com>
2015-06-09 16:22:25 +0200

    add comment

--- update-images-protonet.sh
+++ update-images-protonet.sh
@@ -7,6 +7,9 @@ update_image() {
 
   $DOCKER pull $image:latest
   for layer in $(docker history --no-trunc $image:latest | tail -n +2 | awk '{ print $1 }'); do
+    # This is the most stupid way to check if all layer were downloaded correctly.
+    # But it is the fastest one. The docker save command takes about 30 Minutes for all images,
+    # even with output piped to /dev/null.
     if [[ ! -e /var/lib/docker/overlay/$layer || ! -e /var/lib/docker/graph/$layer ]]; then
       echo "Layer '$layer' of '$image' missing. Switching to previous version."
       $DOCKER tag -f $image:previous $image:latest
