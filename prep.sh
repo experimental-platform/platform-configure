d787aebb1407c4b51dc1a8c9ed53bdf3715d649a
Andreas Litt <andreas.litt@gmail.com>
2015-06-11 11:15:49 +0200

    remove all tagged service files and add new one

--- prep.sh
+++ prep.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo -e "\n\nDEBUG START:\n"
-ls -la /data
-echo -e "\n\nDEBUG END\n"
-
-find /data/ ! -name "update-protonet*" -type f -delete -o -type l -delete && find /services/ ! -name "update-protonet*" -type f -exec cp {} /data \;
+grep -HlR --exclude="update-protonet*" '# ExperimentalPlatform' /data/ | xargs rm -rf
+cp /services/ /data/
 
 cp /update-images-protonet.sh /data/update-images-protonet.sh
