b1bd0c26b6a49977447b48cda805b47da04e5f16
Andreas Litt <andreas.litt@gmail.com>
2015-06-11 11:30:40 +0200

    I hate cp <_<

--- prep.sh
+++ prep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 grep -HlR --exclude="update-protonet*" '# ExperimentalPlatform' /data/ | xargs rm -rf
-cp -R /services/ /data/
+cp /services/* /data/
 
 cp /update-images-protonet.sh /data/update-images-protonet.sh
