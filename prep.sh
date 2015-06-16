45ebd0f11fa32ea4e828ae6716ff8e1fba207db3
Andreas Litt <andreas.litt@gmail.com>
2015-06-16 17:40:53 +0200

    better update

--- prep.sh
+++ prep.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # First remove broken links, this should avoid confusing error messages
 find -L /etc/systemd/system/ -type l -exec rm -f {} +
-grep -HlR --exclude="update-protonet.service" '# ExperimentalPlatform' /data/ | xargs rm -rf
+grep -Hlr --exclude="update-protonet.service" '# ExperimentalPlatform' /data/ | xargs rm -rf
+# do it again to remove garbage
+find -L /etc/systemd/system/ -type l -exec rm -f {} +
 cp /services/* /data/
 
 cp /update-images-protonet.sh /data/update-images-protonet.sh
