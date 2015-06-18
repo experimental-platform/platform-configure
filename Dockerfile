14714f58d47850fdfcacdd1185d21a089fdef115
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-18 10:01:17 +0200

    `/data` now has mounted `/etc` instead of `etc/systemd/system` and we're now copying stuff from the `stuff` folder, as it got a bit unwieldy with multiple files in the docker images root directory.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM dockerregistry.protorz.net/ubuntu:latest
 
 ADD services /services
-ADD update-images-protonet.sh /update-images-protonet.sh
-ADD update-protonet.sh /update-protonet.sh
+ADD stuff /stuff
 ADD prep.sh /prep.sh
 
 CMD [ "/prep.sh" ]
--- prep.sh
+++ prep.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # First remove broken links, this should avoid confusing error messages
-find -L /etc/systemd/system/ -type l -exec rm -f {} +
-grep -Hlr --exclude="update-protonet.service" '# ExperimentalPlatform' /data/ | xargs rm -rf
+find -L /data/systemd/system/ -type l -exec rm -f {} +
+grep -Hlr --exclude="update-protonet.service" '# ExperimentalPlatform' /data/systemd/system/ | xargs rm -rf
 # do it again to remove garbage
-find -L /etc/systemd/system/ -type l -exec rm -f {} +
-cp /services/* /data/
+find -L /data/systemd/system/ -type l -exec rm -f {} +
+cp /services/* /data/systemd/system/
 
-cp /update-images-protonet.sh /data/update-images-protonet.sh
-cp /update-protonet.sh /data/update-protonet.sh
+cp /stuff/update-images-protonet.sh /data/systemd/system/update-images-protonet.sh
+cp /stuff/update-protonet.sh /data/systemd/system/update-protonet.sh
+cp /stuff/journald_protonet.conf /data/systemd/journald.conf.d/journald_protonet.conf
