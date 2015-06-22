1f7070b501379cb71bd2a5194357e284f30e04cf
Andreas Litt <andreas.litt@gmail.com>
2015-06-22 10:29:27 +0200

    add templating script to ci-build

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-set -e
+VERSION=${VERSION:=development}
 
-env
+if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
+  SERVICE_FILE=services/${SERVICE_NAME#platform-}-protonet.service
+
+  if [ -e $SERVICE_FILE ]; then
+    echo "{\"tag\":\"$SERVICE_TAG\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
+    mv $SERVICE_FILE.new $SERVICE_FILE
+  fi
+fi
+
+for SERVICE_FILE in services/*
+do
+  echo "{\"tag\":\"$VERSION\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
+  mv $SERVICE_FILE.new $SERVICE_FILE
+done
+
+git status
 
 exit 1
