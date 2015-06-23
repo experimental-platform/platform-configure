75ea6a14b105b3c4188ae40e4feb50e7441f8556
Andreas Litt <andreas.litt@gmail.com>
2015-06-23 09:50:56 +0200

    ci build test

---
+++ ci-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+VERSION=${VERSION:=development}
+
+if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
+  SERVICE_FILE=services/${SERVICE_NAME#platform-}-protonet.service
+
+  if [ -e $SERVICE_FILE ]; then
+    echo "{\"tag\":\"$SERVICE_TAG\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
+    mv $SERVICE_FILE.new $SERVICE_FILE
+  fi
+fi
+
+for SERVICE_FILE in services/*
+do
+  echo "{\"tag\":\"$VERSION\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
+  mv $SERVICE_FILE.new $SERVICE_FILE
+done
+
+env
+git diff
+
+exit 1
