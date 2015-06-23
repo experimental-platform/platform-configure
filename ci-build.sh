bcc329c654eb30f3b6b7db32df6047f59ed9aab3
Andreas Litt <andreas.litt@gmail.com>
2015-06-23 11:43:22 +0200

    add debug and failure exit for testing purpose

--- ci-build.sh
+++ ci-build.sh
@@ -18,6 +18,9 @@ do
   mv $SERVICE_FILE.new $SERVICE_FILE
 done
 
+env
+exit 1
+
 # Cannot push with deploy key :)
 # git commit --all --message="Jenkins Build $BUILD_NUMBER for $VERSION." --author="Jack Jenkins <jenkins@protonet.info>"
 # git push origin HEAD:build/$VERSION/$BUILD_NUMBER
