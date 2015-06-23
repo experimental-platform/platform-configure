30b4b5619c0c6703f82a1f030a8f91aaf07d0659
Andreas Litt <andreas.litt@gmail.com>
2015-06-23 13:55:36 +0200

    set service_tag correctly + update git_branch

--- ci-build.sh
+++ ci-build.sh
@@ -2,6 +2,7 @@
 set -ex
 # Current branch name will be default Version if nothing else is set
 VERSION=${VERSION:=${GIT_BRANCH#*/}}
+SERVICE_TAG=${SERVICE_TAG#*/}
 
 if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
   SERVICE_FILE=services/${SERVICE_NAME#platform-}-protonet.service
@@ -18,8 +19,12 @@ do
   mv $SERVICE_FILE.new $SERVICE_FILE
 done
 
+if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
+  # this configure build image needs to be tagged with SERVICE_TAG
+  GIT_BRANCH=$SERVICE_TAG
+fi
+
 env
-exit 1
 
 # Cannot push with deploy key :)
 # git commit --all --message="Jenkins Build $BUILD_NUMBER for $VERSION." --author="Jack Jenkins <jenkins@protonet.info>"
