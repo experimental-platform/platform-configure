319c5d56a7b7c3e9ad4d545c28e7b1cf070d451e
Andreas Litt <andreas.litt@gmail.com>
2015-06-23 16:11:58 +0200

    clean up prep.sh

--- prep.sh
+++ prep.sh
@@ -3,30 +3,18 @@
 mkdir -p /data/systemd/system/
 # First remove broken links, this should avoid confusing error messages
 find -L /data/systemd/system/ -type l -exec rm -f {} +
-grep -Hlr --exclude="update-protonet.service" '# ExperimentalPlatform' /data/systemd/system/ | xargs rm -rf
+grep -Hlr '# ExperimentalPlatform' /data/systemd/system/ | xargs rm -rf
 # do it again to remove garbage
 find -L /data/systemd/system/ -type l -exec rm -f {} +
-
-# Set single service to special version
-if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
-  SERVICE_FILE=/services/${SERVICE_NAME#platform-}-protonet.service
-
-  if [ -e $SERVICE_FILE ]; then
-    echo "{\"tag\":\"$SERVICE_TAG\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
-    mv $SERVICE_FILE.new $SERVICE_FILE
-  fi
-fi
-
-# all other files get VERSION as tag
-for SERVICE_FILE in /services/*
-do
-  echo "{\"tag\":\"$VERSION\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
-  mv $SERVICE_FILE.new $SERVICE_FILE
-done
 
 cp /services/* /data/systemd/system/
 
 mkdir -p /data/systemd/system/scripts/
-cp /stuff/update-images-protonet.sh /data/systemd/system/scripts/update-images-protonet.sh
-cp /stuff/update-protonet.sh /data/systemd/system/scripts/update-protonet.sh
+cp /stuff/platform-configure.sh /data/systemd/system/scripts/platform-configure.sh
+
+if [ -d /host-bin/ ]; then
+  # this needs to be the full path on host, not in container
+  ln -sf /etc/systemd/system/scripts/platform-configure.sh /host-bin/platform-configure
+fi
+
 mkdir -p /data/systemd/journald.conf.d && cp /stuff/journald_protonet.conf /data/systemd/journald.conf.d/journald_protonet.conf
