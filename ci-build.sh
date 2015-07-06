6cd7043bdeb35c4b4565560ce435622522215f7e
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-07-06 16:44:45 +0200

    Style guide applied.

--- ci-build.sh
+++ ci-build.sh
@@ -16,16 +16,16 @@ ${DEBUG} && echo -e "(DEBUG) CHANNEL: ${CHANNEL}\n\n"
 if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
   SERVICE_FILE=services/${SERVICE_NAME#platform-}-protonet.service
 
-  if [ -e $SERVICE_FILE ]; then
-    echo "{\"tag\":\"$SERVICE_TAG\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
-    mv $SERVICE_FILE.new $SERVICE_FILE
+  if [ -e ${SERVICE_FILE} ]; then
+    echo "{\"tag\":\"$SERVICE_TAG\"}" | mustache - ${SERVICE_FILE} > ${SERVICE_FILE}.new
+    mv ${SERVICE_FILE}.new ${SERVICE_FILE}
   fi
 fi
 
 for SERVICE_FILE in services/*
 do
-  echo "{\"tag\":\"$VERSION\"}" | mustache - $SERVICE_FILE > $SERVICE_FILE.new
-  mv $SERVICE_FILE.new $SERVICE_FILE
+  echo "{\"tag\":\"${VERSION}\"}" | mustache - ${SERVICE_FILE} > ${SERVICE_FILE}.new
+  mv ${SERVICE_FILE}.new ${SERVICE_FILE}
 done
 
 # Download current version of platform-configure.sh
@@ -37,5 +37,5 @@ if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
 fi
 
 # Cannot push with deploy key :)
-# git commit --all --message="Jenkins Build $BUILD_NUMBER for $VERSION." --author="Jack Jenkins <jenkins@protonet.info>"
-# git push origin HEAD:build/$VERSION/$BUILD_NUMBER
+# git commit --all --message="Jenkins Build ${BUILD_NUMBER} for ${VERSION}." --author="Jack Jenkins <jenkins@protonet.info>"
+# git push origin HEAD:build/${VERSION}/${BUILD_NUMBER}
