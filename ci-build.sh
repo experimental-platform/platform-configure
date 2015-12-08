d5b200e5f727176b2fb578d478b1df779789824c
Hinnerk Haardt <haardt@information-control.de>
2015-12-08 12:03:53 +0100

    Build `platform-configure` if itself is on a feature branch.

--- ci-build.sh
+++ ci-build.sh
@@ -12,24 +12,33 @@ SERVICE_TAG=${SERVICE_TAG:=$TRAVIS_BRANCH}
 # SERVICE_NAME is the name of a service on a feature branch
 
 
-echo -e "\nBuilding platform-configure version '${VERSION}', service_tag '${SERVICE_TAG}', service_name '${SERVICE_NAME}', channel '${CHANNEL}'.\n\n"
+echo -e "\nBuilding platform-configure VERSION '${VERSION}', SERVICE_TAG '${SERVICE_TAG}', SERVICE_NAME '${SERVICE_NAME}', CHANNEL '${CHANNEL}'.\n\n"
 
 # CASE 1: We build an exciting new (feature) branch. That means:
 # 1. there is one systemd service file (SERVICE_NAME) that links to the special docker tag SERVICE_TAG (a.k.a. the branch name)
 # 2. the docker image for platform-configure (this project) will be tagged with SERVICE_TAG
 if [ ! -z "$SERVICE_NAME" ] && [ ! -z "$SERVICE_TAG" ]; then
-  SERVICE_FILE=services/${SERVICE_NAME}-protonet.service
-  if [ -e ${SERVICE_FILE} ]; then
-    echo "{\"tag\":\"$SERVICE_TAG\"}" | mustache - ${SERVICE_FILE} > ${SERVICE_FILE}.new
-    mv ${SERVICE_FILE}.new ${SERVICE_FILE}
-  fi
+    SERVICE_FILE=services/${SERVICE_NAME}-protonet.service
+    if [ -e ${SERVICE_FILE} ]; then
+        echo -e "\n\nBuilding '${SERVICE_FILE}' with TAG '${SERVICE_TAG}':"
+        echo "{\"tag\":\"$SERVICE_TAG\"}" | mustache - ${SERVICE_FILE} > ${SERVICE_FILE}.new
+        mv ${SERVICE_FILE}.new ${SERVICE_FILE}
+    fi
 fi
 
-# CASE 2: Build everything else on a boring old branch, (usually 'development').
+# CASE 2: We're building platform-configure itself on a (feature) branch
+available_channels="development alpha beta stable"
+if [[ -z ${SERVICE_NAME} ]] && [[ -z ${CHANNEL} ]] && [[ ${VERSION} == ${SERVICE_TAG} ]] && [[ ! ${available_channels} =~ ${VERSION} ]]; then
+    echo -e "\nplatform-configure feature branch build detected, setting VERSION to 'development'"
+    VERSION=development
+fi
+
+# CASE 3: Build everything else on a boring old branch, (usually 'development').
 for SERVICE_FILE in services/*
 do
-  echo "{\"tag\":\"${VERSION}\"}" | mustache - ${SERVICE_FILE} > ${SERVICE_FILE}.new
-  mv ${SERVICE_FILE}.new ${SERVICE_FILE}
+    echo -e "\n\nBuilding '${SERVICE_FILE}' with VERSION '${VERSION}':"
+    echo "{\"tag\":\"${VERSION}\"}" | mustache - ${SERVICE_FILE} > ${SERVICE_FILE}.new
+    mv ${SERVICE_FILE}.new ${SERVICE_FILE}
 done
 
 
