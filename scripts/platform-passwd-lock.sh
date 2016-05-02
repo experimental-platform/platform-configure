03f2a7a70dcb284725ab2ffa5e7491b686016199
Hinnerk Haardt <haardt@information-control.de>
2016-05-02 15:35:25 +0200

    Fixes the `unbound variable` error.

--- scripts/platform-passwd-lock.sh
+++ scripts/platform-passwd-lock.sh
@@ -13,21 +13,20 @@ if [[ -f ${PASSWD_LOCK_FILE} ]]; then
     passwd -l ${SYSTEM_USER} | cat
     status=${PIPESTATUS[0]}
     rm -f ${PASSWD_LOCK_FILE}
-fi
 
-
-if [[ ${status} == 0 ]]; then
-    rm -f ${ERROR_FILE} || true
-    echo 'Well done!' > ${SUCCESS_FILE}
-    logger -p INFO -s "Successfully locked account ${SYSTEM_USER}."
-    exit 0
-else
-    logger -s -p ERROR "An ERROR occurred locking account ${SYSTEM_USER}."
-    if [[ -s ${ERROR_FILE} ]]; then
-        logger -p ERROR -s -e -f ${ERROR_FILE}
+    if [[ ${status} == 0 ]]; then
+        rm -f ${ERROR_FILE} || true
+        echo 'Well done!' > ${SUCCESS_FILE}
+        logger -p INFO -s "Successfully locked account ${SYSTEM_USER}."
+        exit 0
     else
-        echo 'Some error occurred.' > ${ERROR_FILE}
+        logger -s -p ERROR "An ERROR occurred locking account ${SYSTEM_USER}."
+        if [[ -s ${ERROR_FILE} ]]; then
+            logger -p ERROR -s -e -f ${ERROR_FILE}
+        else
+            echo 'Some error occurred.' > ${ERROR_FILE}
+        fi
+        rm -f ${SUCCESS_FILE} || true
+        exit 23
     fi
-    rm -f ${SUCCESS_FILE} || true
-    exit 23
 fi
