aa5a915406ae509b80dd14cafd0e6183d6c497e6
Hinnerk Haardt <haardt@information-control.de>
2016-06-20 11:25:23 +0200

    Refactoring and showing the user what's actually going on.

--- scripts/self_destruct.sh
+++ scripts/self_destruct.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+ERROR=false
+
 list_subsystems() {
 	local DEVPATH
 	DEVPATH="$1"
@@ -17,6 +19,7 @@ list_subsystems() {
 	return
 }
 
+
 is_hotpluggable() {
 	local DEVICE
 	DEVICE="$(realpath /sys/block/$1)"
@@ -31,6 +34,7 @@ is_hotpluggable() {
 	return 1
 }
 
+
 enable_ignition() {
     local IGNITION_UUID="00000000-0000-0000-0000-000000000001"
     local ROOTDEV=$(blkid -L ROOT | grep -oP "[/a-zA-Z]*")
@@ -38,13 +42,15 @@ enable_ignition() {
     (echo x; echo g; echo ${IGNITION_UUID}; echo w; echo y;) | gdisk ${ROOTDEV} > /dev/null || true
 }
 
+
 remove_config() {
     grep -Hlr '# ExperimentalPlatform' /etc/systemd/system/ | xargs --no-run-if-empty rm -rf
     find -L /etc/systemd/system/ -type l -exec rm -f {} +
     rm -rf /etc/protonet
     rm -f /etc/ssh/ssh_host_*
-    userdel -f -r platform
+    userdel -f -r platform &> /dev/null || true
 }
+
 
 unlabel_drives() {
 	ROOTDEV=$(lsblk --noheadings --output PKNAME $(blkid -L ROOT))
@@ -53,57 +59,87 @@ unlabel_drives() {
 		DEVNAME="$(basename "$d")"
 
 		if [ "$DEVNAME" == "$ROOTDEV" ]; then
-			echo " * Drive '$DEVNAME' is the boot device - skipping it"
+			echo -e "\t* Drive '$DEVNAME' is the boot device - skipping it"
 			continue
 		fi
 
 		if [ "$(</sys/block/$DEVNAME/removable)" -eq 1 ]; then
-			echo " * Drive '$DEVNAME' is removable - skipping it"
+			echo -e "\t* Drive '$DEVNAME' is removable - skipping it"
 			continue
 		fi
 
 		if is_hotpluggable $DEVNAME; then
-			echo "Drive '$DEVNAME' is hot-pluggable - skipping it"
+			echo -e "\t* Drive '$DEVNAME' is hot-pluggable - skipping it"
 			continue
 		fi
 
-		echo " * Clearing ZFS labels and GPT on drive '$DEVNAME'"
-		zpool labelclear -f "$DEVNAME" || true
-		dd if=/dev/zero "of=$DEVNAME" bs=512 count=20
+		echo -ne "\t* Clearing ZFS labels and GPT on drive '$DEVNAME'... "
+		zpool labelclear -f "$DEVNAME" &>/dev/null || true
+		if dd if=/dev/zero "of=$DEVNAME" bs=512 count=20 &>/dev/null; then
+		    echo "OKAY"
+		else
+		    echo "ERROR!"
+		    ERROR=true
+		fi
 	done
 }
 
-ZFS_MOUNTPOINTS=$(mount | awk '/zfs/ {print $3}')
 
-rm -f /etc/zfs/zpool.cache
-systemctl stop docker.service
-systemctl stop systemd-journald-audit.socket
-systemctl stop systemd-journald-dev-log.socket
-systemctl stop systemd-journald.socket
-systemctl stop systemd-journald.service && zfs umount -f protonet_storage/journal || true
+remove_zfs() {
+    rm -f /etc/zfs/zpool.cache
+    systemctl stop docker.service
+    systemctl stop systemd-journald-audit.socket
+    systemctl stop systemd-journald-dev-log.socket
+    systemctl stop systemd-journald.socket
+    systemctl stop systemd-journald.service && zfs umount -f protonet_storage/journal || true
 
-# try to destroy the zpool only if it actually exists
-if zpool list -H | grep -qE ^protonet_storage; then
-	zpool destroy -f protonet_storage
-fi
+    # try to destroy the zpool only if it actually exists
+    if zpool list -H | grep -qE ^protonet_storage; then
+        zpool destroy -f protonet_storage
+    fi
+}
 
-unlabel_drives
+
+remove_mountpoints() {
+    local CURRENT_MOUNTPOINTS=$(mount)
+    local MOUNTPOINT
+    for MOUNTPOINT in  /home /data /var/lib/docker /var/log/journal; do
+        # only remove it if it's not mounted any more
+        echo -ne "\t* REMOVING '${MOUNTPOINT}'... "
+        if  [[ ! "${CURRENT_MOUNTPOINTS}" =~ "${MOUNTPOINT}" ]] && rm -rf $MOUNTPOINT; then
+            echo "OKAY"
+        else
+            echo "ERROR: PLEASE FIX ${MOUNTPOINT} MANUALLY"
+            ERROR=true
+        fi
+    done
+}
+
+
+echo -n "REMOVING ZFS POOL... "
+remove_zfs && echo "OKAY."
+
+echo "UNLABELING DRIVES... "
+unlabel_drives && echo "OKAY"
 sync; sync; sync
-# remove the config only after the ZFS pool was destroyed successfully.
-remove_config
 
-# ignition useradd is not idempotent, so let's wait until after the user has been deleted
-enable_ignition
+# remove the config only after the ZFS pool was destroyed successfully.
+echo -n "REMOVING CUSTOM SYSTEM CONFIG... "
+remove_config && echo "OKAY"
 
 echo "REMOVING ZFS MOINTPOINTS..."
-CURRENT_MOUNTPOINTS=$(mount)
-for MOUNTPOINT in ${ZFS_MOUNTPOINTS}; do
-    # only remove it if it's not mounted any more
-    if  [[ ! "${CURRENT_MOUNTPOINTS}" =~ "${MOUNTPOINT}" ]] && rm -rf $MOUNTPOINT; then
-        echo -e "\tOKAY, ${MOUNTPOINT} removed"
-    else
-        echo -e "ERROR: PLEASE FIX ${MOUNTPOINT} MANUALLY"
-    fi
-done
+remove_mountpoints && echo "OKAY"
 
-echo "Destruction successful"
+# ignition useradd is not idempotent, so let's wait until after the user has been deleted
+echo -n "ENABLING IGNITION... "
+enable_ignition && echo "OKAY"
+
+
+if [[ "${ERROR}"==true ]]; then
+    echo "ERRORS detected, please fix manually!"
+    exit 23
+else
+    echo "Destruction successful"
+fi
+
+
