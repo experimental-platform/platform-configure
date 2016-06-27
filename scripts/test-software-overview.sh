21ec825f1e46fc61f62b0060810942b367ed4044
Hinnerk Haardt <haardt@information-control.de>
2016-06-27 17:26:52 +0200

    This test just doesn't work without parsing the systemd units in full.

--- scripts/test-software-overview.sh
+++ scripts/test-software-overview.sh
@@ -5,25 +5,6 @@ set -eu
 ERROR_CODE=0
 ERROR="\x1b[93;41mERROR\x1b[0m"
 
-
-echo -n "Test the number of running services... "
-
-EP="# ExperimentalPlatform"
-DR="docker run"
-TO="Type=oneshot"
-WHERE="/etc/systemd/system/"
-
-SHOULD=$(grep -Hlr "${EP}" "${WHERE}" | xargs grep -Hlr "${DR}" "${WHERE}" | xargs grep -Hlr "${TO}" "${WHERE}" | wc -l)
-ACTUALLY=$(docker ps | grep -P "quay.io/(experimentalplatform|protonetinc)" | wc -l)
-
-if [[ "${SHOULD}" -eq "${ACTUALLY}" ]]; then
-    echo "OKAY, (${SHOULD}) running."
-else
-    echo -e "${ERROR}: Expected ${SHOULD} services but ${ACTUALLY} are running."
-    ERROR_CODE=23
-fi
-
-
 echo -n "Test for identical version... "
 TAGS=$(docker ps | awk '/^[0-9a-z]+/ { split($2, a, ":"); print a[2]}' | uniq)
 NUM_TAGS=$(wc -l <<< ${TAGS})
