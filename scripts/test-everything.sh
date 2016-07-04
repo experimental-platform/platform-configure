488044375f976ec1510851c09a3a94b7fa7436d7
Kamil Domański <kamil@domanski.co>
2016-07-04 17:15:01 +0200

    make 'test-everything' return JSON

--- scripts/test-everything.sh
+++ scripts/test-everything.sh
@@ -4,6 +4,22 @@ set -eu
 
 EXIT_CODE=0
 ERROR="\x1b[93;41mERROR\x1b[0m"
+JSON={}
+JSON_OUTPUT=false
+
+while [[ $# > 0 ]]; do
+  key="$1"
+  case $key in
+    --json)
+      JSON_OUTPUT=true
+    ;;
+    *)
+      echo "Unknown parameter '$key'"
+      exit 1
+    ;;
+  esac
+  shift
+done
 
 test_soul() {
     # SOUL_URL=http://172.17.0.1 # Note: We should set this automatically to DOCKERHOST ip
@@ -42,13 +58,24 @@ run_tests() {
     # TODO: add test_soul
     for testname in test-disks test-ipmi-disabled test-software-overview; do
         if [[ -x "/etc/systemd/system/scripts/${testname}.sh" ]]; then
-            if ! /etc/systemd/system/scripts/${testname}.sh; then
-                EXIT_CODE=42
-                echo -e "\n\n"
+            OUTFILE=$(mktemp)
+            /etc/systemd/system/scripts/${testname}.sh > "$OUTFILE" || true
+            STATUS=${PIPESTATUS[0]}
+            OUTPUT="$(<$OUTFILE)"
+            [ "$JSON_OUTPUT" != "true" ] && echo "$OUTPUT"
+            if [ $STATUS -eq 0 ]; then
+              STATUS="ok"
+            else
+              STATUS="failed"
+              EXIT_CODE=23
+              [ "$JSON_OUTPUT" != "true" ] && echo -e "\n\n"
             fi
+
+            JSON="$(jq --arg testname "$testname" --arg status "$STATUS" --arg output "$OUTPUT" '.tests[$testname].status = $status | .tests[$testname].output = $output' <<< "$JSON")"
         else
-            echo -e "${ERROR}: Test \"${testname}\" not found!"
+            [ "$JSON_OUTPUT" != "true" ] && echo -e "${ERROR}: Test \"${testname}\" not found!"
             EXIT_CODE=23
+            JSON="$(jq --arg testname "$testname" '.tests.status[$testname] = "not-found"' <<< "$JSON")"
         fi
     done
 }
@@ -59,28 +86,37 @@ button rainbow
 run_tests
 trap - SIGINT SIGTERM EXIT
 
-echo -e "HARDWARE INFO\n"
+
 HWINFO=$(sudo /etc/systemd/system/scripts/test-hwinfo.sh)
-echo -e "MAINBOARD:"
-jq ' .motherboard | "Vendor: \(.vendor)    Name: \(.name)    Version: \(.version)    Serial: \(.serial)"' --raw-output <<< ${HWINFO}
-echo -e "\nRAM:"
-jq ' .ram | map("Vendor: \(.vendor)    Slot: \(.slot)    Size: \(.size)    Product: \(.product)    Serial: \(.serial)")' --raw-output <<< ${HWINFO}
-echo -e "\nHARD DISKS (and USB Sticks):"
-jq ' .drives | map("Vendor: \(.vendor)    Model: \(.model)    Size: \(.size)    Serial: \(.serial)")' --raw-output <<< ${HWINFO}
-echo -e "\nMAC ADDRESSES:"
-jq '.network | map(select(.name | startswith("veth") | not) | select(.name != "docker0") | select(.name != "lo") | select(.name | startswith("br-") | not) | "\(.name): \(.mac)")[]' --raw-output <<< ${HWINFO}
+if [ "$JSON_OUTPUT" != "true" ]; then
+  echo -e "HARDWARE INFO\n"
+  echo -e "MAINBOARD:"
+  jq ' .motherboard | "Vendor: \(.vendor)    Name: \(.name)    Version: \(.version)    Serial: \(.serial)"' --raw-output <<< ${HWINFO}
+  echo -e "\nRAM:"
+  jq ' .ram | map("Vendor: \(.vendor)    Slot: \(.slot)    Size: \(.size)    Product: \(.product)    Serial: \(.serial)")' --raw-output <<< ${HWINFO}
+  echo -e "\nHARD DISKS (and USB Sticks):"
+  jq ' .drives | map("Vendor: \(.vendor)    Model: \(.model)    Size: \(.size)    Serial: \(.serial)")' --raw-output <<< ${HWINFO}
+  echo -e "\nMAC ADDRESSES:"
+  jq '.network | map(select(.name | startswith("veth") | not) | select(.name != "docker0") | select(.name != "lo") | select(.name | startswith("br-") | not) | "\(.name): \(.mac)")[]' --raw-output <<< ${HWINFO}
 
-echo -e "\nSOFTWARE CHANNEL, BOOT STICK BUILD AND SUPPORT IDENTIFIER:"
-jq ' "SYSTEM CHANNEL: \(.channel)"' --raw-output <<< ${HWINFO}
-jq ' "BOOTSTICK CHANNEL: \(.bootstick.BRANCH)"' --raw-output <<< ${HWINFO}
-jq ' "BOOTSTICK BUILD: \(.bootstick.BUILD)"' --raw-output <<< ${HWINFO}
-jq ' "SUPPORT IDENTIFIER: \(.support_identifier)"' --raw-output <<< ${HWINFO}
+  echo -e "\nSOFTWARE CHANNEL, BOOT STICK BUILD AND SUPPORT IDENTIFIER:"
+  jq ' "SYSTEM CHANNEL: \(.channel)"' --raw-output <<< ${HWINFO}
+  jq ' "BOOTSTICK CHANNEL: \(.bootstick.BRANCH)"' --raw-output <<< ${HWINFO}
+  jq ' "BOOTSTICK BUILD: \(.bootstick.BUILD)"' --raw-output <<< ${HWINFO}
+  jq ' "SUPPORT IDENTIFIER: \(.support_identifier)"' --raw-output <<< ${HWINFO}
+fi
 
+JSON="$(jq --argjson hwinfo "$HWINFO" '.hwinfo = $hwinfo' <<< "$JSON")"
 
 if [[ "${EXIT_CODE}" -eq "0" ]]; then
     button hdd
-    echo -e "\n\nOKAY -- OKAY -- OKAY\nALL TESTS SUCCESSFUL\n"
+    [ "$JSON_OUTPUT" != "true" ] && echo -e "\n\nOKAY -- OKAY -- OKAY\nALL TESTS SUCCESSFUL\n"
 else
     button error
-    echo -e "\n\n${ERROR}: A TEST WENT WRONG, PLEASE INVESTIGATE"
+    [ "$JSON_OUTPUT" != "true" ] && echo -e "\n\n${ERROR}: A TEST WENT WRONG, PLEASE INVESTIGATE"
 fi
+
+if [ "$JSON_OUTPUT" == "true" ]; then
+  echo "$JSON"
+fi
+
