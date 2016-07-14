49c82c84525f67ac03558bf31fa440d4f7f7d5f5
Hinnerk Haardt <haardt@information-control.de>
2016-07-14 13:22:38 +0200

    Shorter support_id; should always use the same mac address if the hardware does not change. [Fixes #126204405]

--- scripts/platform-identifier.sh
+++ scripts/platform-identifier.sh
@@ -6,13 +6,6 @@ SUPPORT_FILE="/etc/protonet/system/support_identifier"
 BOX_NAME_FILE="/etc/protonet/box_name"
 
 
-get_default_mac() {
-    local INTERFACE=$(netstat -nr | awk '/^0\.0\.0\.0/ { print $8 }')
-    local MAC=$(ip link show ${INTERFACE} | awk '/link/ { gsub(/:/, "", $2); print toupper($2)}')
-    echo -n "${MAC}"
-}
-
-
 get_macs() {
     local interfaces=$(ip link show | awk '/^[0-9]+:\s+[ew][a-z0-9]+/ { gsub(/:/, "", $2); print $2 }')
     local mac
@@ -20,18 +13,27 @@ get_macs() {
     local iface
     for iface in ${interfaces}; do
         mac=$(ip link show ${iface} | awk '/link/ { gsub(/:/, "", $2); print toupper($2)}')
-        result=$(echo ${result}-${mac})
+        if [[ -z "$result" ]]; then
+            result="$mac"
+        else
+            result="${result}\\n${mac}"
+        fi
     done
-    echo -n "${result}"
+    echo -e "$result"
+}
+
+
+get_default_mac() {
+    echo -n $(get_macs | sort | head -n 1)
 }
 
 
 get_hw() {
     local INTERFACE_COUNT=$(ip link show | grep -P '^\d+:\s+[ew][a-z0-9]+'| wc -l)
     if [[ "${INTERFACE_COUNT}" -eq 2 ]]; then
-        echo -n "MAYA"
+        echo -n "M"
     else
-        echo -n "CARLA"
+        echo -n "C"
     fi
 }
 
@@ -46,7 +48,7 @@ main () {
         echo -n "Writing support identifier to ${SUPPORT_FILE}... "
         mkdir -p $(dirname "${SUPPORT_FILE}")
         get_hw > ${SUPPORT_FILE}
-        get_macs >> ${SUPPORT_FILE}
+        get_default_mac >> ${SUPPORT_FILE}
         echo >> ${SUPPORT_FILE}
         echo "DONE"
     else
