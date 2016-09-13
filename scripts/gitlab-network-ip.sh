976d6300fb05b9a2afb8d96593cc9e07864d75cd
Kamil Domański <kamil@domanski.co>
2016-09-13 11:32:27 +0200

    replace haproxy with new central-gateway

--- scripts/gitlab-network-ip.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Write out gitlab-network ip to SKVS if it has changed
-if [ "$(/opt/bin/gitlab-network show | md5sum)" != "$(cat /etc/protonet/gitlab/ip | md5sum)" ]; then
-  /opt/bin/gitlab-network show > /etc/protonet/gitlab/ip
-  echo "Wrote gitlab-network ip to SKVS since it changed"
-fi
