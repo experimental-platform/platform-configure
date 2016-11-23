b482766ebc0886ee7ab40b65560f73a10882a79a
Kamil Domański <kamil@domanski.co>
2016-11-23 14:00:22 +0100

    test-stress: use ubuntu, install stress first

--- scripts/test-stress.sh
+++ scripts/test-stress.sh
@@ -5,6 +5,6 @@ set -eu
 ERROR="\x1b[93;41mERROR\x1b[0m"
 echo "RUNNING STRESS TEST... "
 
-toolbox --bind=/dev:/dev stress -t 10 -d 20 -i 20 -c 20 --vm 20
+docker run -it --rm -v /dev:/dev quay.io/experimentalplatform/ubuntu:latest bash -c "apt-get update -qq && apt-get install -q -y stress && stress -t 10 -d 20 -i 20 -c 20 --vm 20"
 
 echo "OKAY"
