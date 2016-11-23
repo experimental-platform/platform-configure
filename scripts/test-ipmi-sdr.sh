664afef4291afbec8851079783778eccae2ebc79
Kamil Domański <kamil@domanski.co>
2016-11-23 16:19:07 +0100

    test-ipmi-sdr: run only if IPMI present

--- scripts/test-ipmi-sdr.sh
+++ scripts/test-ipmi-sdr.sh
@@ -5,5 +5,9 @@ set -eu
 ERROR="\x1b[93;41mERROR\x1b[0m"
 echo "RUNNING IPMI SDR TEST... "
 
-ipmitool sdr
-echo "OKAY"
+if [[ -e /dev/ipmi0 ]] || [[ -e /dev/ipmi/0 ]] || [[ -e /dev/ipmidev/0 ]]; then
+	ipmitool sdr
+	echo "OKAY"
+else
+	echo "OKAY, IPMI NOT AVAILABLE."
+fi
