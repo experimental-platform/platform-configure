496c05fdf36a173887db4dc001bad5e1204d9ae6
Kamil Domański <kamil@domanski.co>
2016-11-28 16:19:50 +0100

    test-stress: return to using toolbox, but with the dnf SIGKILL workaround

--- scripts/test-stress.sh
+++ scripts/test-stress.sh
@@ -5,6 +5,6 @@ set -eu
 ERROR="\x1b[93;41mERROR\x1b[0m"
 echo "RUNNING STRESS TEST... "
 
-docker run -it --rm -v /dev:/dev quay.io/experimentalplatform/ubuntu:latest bash -c "apt-get update -qq && apt-get install -q -y stress && stress -t 10 -d 20 -i 20 -c 20 --vm 20"
+toolbox --bind=/dev:/dev bash -c '((rpm -qa | grep -q '^stress-') || (rpm --import /etc/pki/rpm-gpg/RPM* && dnf install -q -y stress)) && stress -t 10 -d 20 -i 20 -c 20 --vm 20'
 
 echo "OKAY"
