b4fcb9dd513edf5f9fc9c51103f85b7319009b4f
martin.meier@protonet.info <martin.meier@protonet.info>
2017-02-01 13:10:56 +0100

    :art: puttin the stress values to reasonable values. Should only eat 10GB of discspace while testing now, not 80GB. Should not spawn more cpu workers than cpus in the system 16 max.

--- scripts/test-stress.sh
+++ scripts/test-stress.sh
@@ -5,6 +5,6 @@ set -eu
 ERROR="\x1b[93;41mERROR\x1b[0m"
 echo "RUNNING STRESS TEST... "
 
-toolbox --bind=/dev:/dev bash -c '((rpm -qa | grep -q '^stress-') || (rpm --import /etc/pki/rpm-gpg/RPM* && dnf install -q -y stress)) && stress -t 10 -d 20 -i 20 -c 20 --vm 20'
+toolbox --bind=/dev:/dev bash -c '((rpm -qa | grep -q '^stress-') || (rpm --import /etc/pki/rpm-gpg/RPM* && dnf install -q -y stress)) && stress -t 10 -d 5 -i 5 -c 16 -m 5'
 
 echo "OKAY"
